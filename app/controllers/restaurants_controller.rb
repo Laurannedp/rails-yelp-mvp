f730f6ce43fe680da5996c84f282a7899ea06a46
lauranne depatoul <lauranne.depatoul@gmail.com>
2020-05-13 12:41:12 +0200

    add category to restaurant

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,6 @@
+class RestaurantsController < ApplicationController
+
+    def top
+    @restaurants = Restaurant.where(stars: 5)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_093857) do
+ActiveRecord::Schema.define(version: 2020_05_13_103904) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
     t.integer "phone_number"
-    t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "category"
   end
 
   create_table "reviews", force: :cascade do |t|
